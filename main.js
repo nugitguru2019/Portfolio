83633c982966c12e7c52bf0941e280e265d19ebf
Moh Chica <onibadesh@yahoo.com>
2019-05-05 23:17:25 -0500

    modified portfolio

---
+++ main.js
@@ -0,0 +1,27 @@
+AOS.init();
+
+
+
+/* HTML Variables */
+
+ const main = document.querySelector('main');
+
+ const openingSection = document.getElementById('opening-section');
+
+ const portfolioGrid = document.getElementById('portfolio-grid');
+
+ const cvGrid = document.getElementById('cv-grid');
+
+ const contactGrid = document.getElementById('contact-grid');
+
+
+
+ const aboutBtn = document.getElementById('about-btn');
+
+ const portfolioBtn = document.getElementById('portfolio-btn');
+
+ const cvBtn = document.getElementById('cv-btn');
+
+ const contactBtn = document.getElementById('contact-btn');
+
+
